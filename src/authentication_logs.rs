757a492b3bfffb0d3c8c14e0fb56863dae3b65a5
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2022-05-20 10:53:43 +0200

    concate logins and elevations

--- src/authentication_logs.rs
+++ src/authentication_logs.rs
@@ -30,7 +30,7 @@ pub async fn watch_authentication_logs(dispatcher: &Dispatcher, config: &Config)
         position += file.read_to_end(&mut contents).await.unwrap();
 
         let contents_str = from_utf8(&contents).unwrap();
-        let logins = parse_logins(contents_str);
+        let logins = parse_authentications(contents_str);
 
         for login in logins.iter() {
             info!("logins {:?}", login);
@@ -38,6 +38,36 @@ pub async fn watch_authentication_logs(dispatcher: &Dispatcher, config: &Config)
         }
 
         interval.tick().await;
+    }
+}
+
+#[derive(Debug)]
+struct RootElevation {
+    username: String,
+    datetime: String,
+    hostname: String,
+}
+
+impl RootElevation {
+    fn from_capture(cap: &Captures) -> RootElevation {
+        let username = cap["username"].to_owned();
+        let datetime = cap["datetime"].to_owned();
+        let hostname = cap["hostname"].to_owned();
+        RootElevation {
+            username,
+            datetime,
+            hostname,
+        }
+    }
+}
+
+impl Notification for RootElevation {
+    fn message(&self) -> String {
+        format!(
+            "use <b>{}</b> just become root on <code>{}</code>\n{}",
+            self.username, self.hostname, self.datetime
+        )
+        .to_string()
     }
 }
 
@@ -70,23 +100,41 @@ impl Login {
 impl Notification for Login {
     fn message(&self) -> String {
         format!(
-            "New login by <b>{}</b> from IP <code>{}</code> on <b>{}</b>method: {}\n{}",
-            self.username, self.ip, self.hostname, self.datetime, self.method
+            "New login by <b>{}</b> from IP <code>{}</code> on <b>{}</b>\nmethod: {}\n{}",
+            self.username, self.ip, self.hostname, self.method, self.datetime
         )
         .to_string()
     }
 }
 
-/// Parse a line in the authentication logs into a vector of [Login](Login) entries.
-fn parse_logins(contents: &str) -> Vec<Login> {
+/// Finds logins in authentication logs
+fn find_root_elevations(contents: &str) -> Vec<Login> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(?P<datetime>\D{3,4} \d{1,2} \d{1,2}:\d{2}:\d{2}) (?P<hostname>.+) sudo: pam_unix(sudo:session): session opened for user root by (?P<username>.*)").unwrap();
+    }
+    RE.captures_iter(contents)
+        .map(|cap| Login::from_capture(&cap))
+        .collect()
+}
+
+/// Finds logins in authentication logs
+fn find_logins(contents: &str) -> Vec<Login> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"(?P<datetime>\D{3,4} \d{1,2} \d{1,2}:\d{2}:\d{2}) (?P<hostname>.+) (?P<process>sshd\[\d+\]):.* Accepted (?P<method>\w+) for (?P<username>.*) from (?P<ipaddress>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}) port (?P<port>\d+)").unwrap();
     }
-    let logins = RE
-        .captures_iter(contents)
+    RE.captures_iter(contents)
         .map(|cap| Login::from_capture(&cap))
-        .collect();
-    logins
+        .collect()
+}
+
+/// Parse a line in the authentication logs into a vector of [Login](Login) entries.
+fn parse_authentications(contents: &str) -> Vec<Login> {
+    let mut logins = find_logins(&contents);
+    let mut root_elevations = find_root_elevations(&contents);
+    let mut concatenated = vec![];
+    concatenated.append(&mut logins);
+    concatenated.append(&mut root_elevations);
+    concatenated
 }
 
 #[tokio::test]
@@ -96,6 +144,6 @@ async fn parse_test() {
 
     let test_file = Path::new("test/auth.log");
     let data = fs::read_to_string(test_file).unwrap();
-    let logins = parse_logins(&data);
-    assert_eq!(logins.len(), 2);
+    let logins = parse_authentications(&data);
+    assert_eq!(logins.len(), 3);
 }
