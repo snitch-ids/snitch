c3e2c5335b806326d7883580d8edbc1358c908d9
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2022-08-07 19:26:03 +0200

    add backend connection

---
+++ src/notifiers/backend.rs
@@ -0,0 +1,50 @@
+use reqwest;
+use reqwest::header::HeaderMap;
+use std::collections::HashMap;
+use std::env;
+
+use serde::{Deserialize, Serialize};
+
+use super::Message;
+
+pub async fn send_message(message: Message) -> Result<(), reqwest::Error> {
+    let url = format!("http://127.0.0.1:8080/messages/");
+
+    let mut params = HashMap::new();
+
+    params.insert("hostname", message.hostname);
+    params.insert("timestamp", message.timestamp.to_string());
+    params.insert("title", message.title);
+    params.insert("content", message.content);
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Accept", "application/json".parse().unwrap());
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+
+    debug!("dispatching message");
+
+    let client = reqwest::Client::new();
+    client
+        .post(url)
+        .headers(headers)
+        // .json(&message)  // TODO: why doesnt this work???
+        .json(&params)
+        .send()
+        .await?;
+    debug!("sent message");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::send_message;
+    use crate::test_util::get_test_message;
+
+    /// Tests dispatching message. Requires configured TELEGRAM configuration
+    #[tokio::test]
+    async fn test_send_message() {
+        let message = get_test_message();
+        send_message(message).await.unwrap();
+    }
+}
