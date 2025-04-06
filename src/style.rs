f8399c292641823d8d8ae21e9966fd63bf366fcd
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2025-04-06 11:45:56 +0200

    bump indicatif

--- src/style.rs
+++ src/style.rs
@@ -2,10 +2,11 @@ use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn get_progressbar(n_items: u64, redraw_delta: u64) -> ProgressBar {
     let bar = ProgressBar::new(n_items);
-    bar.set_style(
-        ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"),
-    );
-    bar.set_draw_delta(redraw_delta);
+    let style = ProgressStyle::with_template(
+        "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+    )
+    .unwrap();
+    bar.set_style(style);
+    // bar.set_draw_delta(redraw_delta);
     bar
 }
